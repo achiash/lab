bcd06b5ec744a0be0acd7bd4ab3d53bc9400fab1
Achia Shaag <achia.shaag@gmail.com>
2013-01-31 18:25:49 +0200

    added Crawler.java

---
+++ src/com/social/crawler/Crawler.java
@@ -0,0 +1,27 @@
+package com.social.crawler;
+
+public class Crawler {
+	
+	
+
+	private static final int MAX_USERS_TO_CRAWL = 1000;
+	private static final int TWEETS_PER_USER = 5;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void crawlUsers() {
+		
+	}
+	
+	public static void crawlTweets() {
+		
+	}
+	
+
+}
